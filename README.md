e98cc07d0ac0f3a778b1096be387e8338f3b364b
cmlegault <chris.legault@noaa.gov>
2019-02-20 09:40:39 -0500

    split R code into smaller chunks

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Premise is that missing catch, due to underestimated discards and/or improperly 
 
 A stock assessment that exhibited a strong retrospective pattern was used as a starting point. Catch multipliers were searched for that elimated the retrospective pattern for different starting years. These modified assessments were followed through to catch advice applying an F40% rule to compare with the original assessment and the original assessment with retrospective adjustment.
 
-## Case Study 1: Witch Flounder (SARC 62, 2016)
+### Case Study 1: Witch Flounder (SARC 62, 2016)
 
 The benchmark assessment for witch flounder at SARC 62 rejected the proposed ASAP model due to a strong retrospective pattern (https://www.nefsc.noaa.gov/publications/crd/crd1701/, https://www.nefsc.noaa.gov/publications/crd/crd1703/, and https://www.nefsc.noaa.gov/saw/saw62/sarc62_panel_summary_report.pdf). An ASAP file similar to the final run examined in SARC 62 was used as the starting point for this exercise. This assessment formulation used years 1982-2015, ages 1-11+, one fleet with three selectivity blocks, and five survey tuning indices (NESFS spring and fall scaled by the catchability study, ASMFC summer, and ME/NH spring and fall surveys). This assessment exhibited a strong retrospective pattern with Mohn's rho values of 0.64 and -0.46 for SSB and F, respectively.
 
---
+++ Rcode/04_catch_advice_witch.R
@@ -0,0 +1,112 @@
+source("03_plot_witch_retros.R")
+
+# 04_catch_advice_witch.R
+# calculate catch advice under F40% for base, rho adjusted, and three catch multiplier scenarios
+
+# compare fleet selectivity in final year
+nages <- asap$parms$nages
+ages <- seq(1, nages)
+seldf <- data.frame(Age = rep(ages, 4),
+                    Source = c(rep("Base", nages), 
+                               paste("Change Year", rep(bestres$ChangeYear, each = nages))),
+                    Selectivity = c(asap$fleet.sel.mats[[1]][nyears, ],
+                                    asap1$fleet.sel.mats[[1]][nyears, ],
+                                    asap2$fleet.sel.mats[[1]][nyears, ],
+                                    asap3$fleet.sel.mats[[1]][nyears, ]) )
+
+selplot <- ggplot(seldf, aes(x=Age, y=Selectivity, color=Source)) +
+  geom_point() +
+  geom_line() +
+  expand_limits(y=0) +
+  theme_bw()
+
+print(selplot)
+ggsave(".\\witch\\selplot.png", selplot)
+
+# calculate F40% from ASAPplot
+# do it for each of the four asap runs
+a1 <- list(asap.name = asapfname)
+PlotSPRtable(asap, a1, 5, TRUE, ".\\witch\\", "png")
+sprtab <- read.csv(paste0(".\\witch\\SPR.Target.Table_", asapfname, ".csv"))
+asapF40 <- sprtab$F..SPR.[sprtab$X.SPR == 0.40]
+
+a11 <- list(asap.name = asapcmultfnames[1])
+PlotSPRtable(asap1, a11, 5, TRUE, ".\\witch\\", "png")
+sprtab1 <- read.csv(paste0(".\\witch\\SPR.Target.Table_", asapcmultfnames[1], ".csv"))
+asap1F40 <- sprtab1$F..SPR.[sprtab1$X.SPR == 0.40]
+
+a12 <- list(asap.name = asapcmultfnames[2])
+PlotSPRtable(asap2, a12, 5, TRUE, ".\\witch\\", "png")
+sprtab2 <- read.csv(paste0(".\\witch\\SPR.Target.Table_", asapcmultfnames[2], ".csv"))
+asap2F40 <- sprtab2$F..SPR.[sprtab2$X.SPR == 0.40]
+
+a13 <- list(asap.name = asapcmultfnames[3])
+PlotSPRtable(asap3, a13, 5, TRUE, ".\\witch\\", "png")
+sprtab3 <- read.csv(paste0(".\\witch\\SPR.Target.Table_", asapcmultfnames[3], ".csv"))
+asap3F40 <- sprtab3$F..SPR.[sprtab3$X.SPR == 0.40]
+
+cbind(asapF40, asap1F40, asap2F40, asap3F40)
+
+dev.off()
+
+# do short term projections under F40%
+calcShortTermProj <- function(asap.name, startNAA, recruits, Fmult, nyears){
+  nages <- length(startNAA)
+  vals <- read.csv(paste0(".\\witch\\AGEPRO_ave_params_", asap.name, ".csv"))
+  MAA <- as.numeric(vals[vals$X == "M.age", 2:(nages+1)])
+  matAA <- as.numeric(vals[vals$X == "mat.age", 2:(nages+1)])
+  WAA <- as.numeric(vals[vals$X == "catch.waa", 2:(nages+1)])
+  selx <- as.numeric(vals[vals$X == "sel.age", 2:(nages+1)])
+  FAA <- Fmult * selx
+  ZAA <- MAA + FAA
+  ctot <- rep(NA, nyears)
+  NAA <- matrix(NA, nrow=(nyears+1), ncol=nages)
+  NAA[1, ] <- startNAA
+  NAA[2:(nyears+1), 1] <- recruits
+  for (iyear in 1:nyears){
+    for (iage in 1:(nages-1)){
+      NAA[iyear+1, iage+1] <- NAA[iyear, iage] * exp(-ZAA[iage])
+    }
+    NAA[iyear+1, nages] <- NAA[iyear+1, nages] * exp(-ZAA[nages])
+    ctot[iyear] <- sum(NAA[iyear, ] * WAA* FAA * (1 - exp(-ZAA)) / ZAA)
+  }
+  return(ctot)
+}
+
+nprojyears <- 3
+stp <- calcShortTermProj(asapfname, asap$N.age[nyears, ], mean(asap$N.age[, 1]), asapF40, nprojyears)
+ssbrho <- res$SSBrho[1]
+Nrhoadj <- asap$N.age[nyears, ] / (1 + ssbrho)
+stprhoadj <- calcShortTermProj(asapfname, Nrhoadj, mean(asap$N.age[, 1]), asapF40, nprojyears)
+stp1 <- calcShortTermProj(asapcmultfnames[1], asap1$N.age[nyears, ], mean(asap1$N.age[, 1]), asap1F40, nprojyears)
+stp2 <- calcShortTermProj(asapcmultfnames[2], asap2$N.age[nyears, ], mean(asap2$N.age[, 1]), asap2F40, nprojyears)
+stp3 <- calcShortTermProj(asapcmultfnames[3], asap3$N.age[nyears, ], mean(asap3$N.age[, 1]), asap3F40, nprojyears)
+
+cmult <- as.numeric(substr(asapcmultfnames, 7, 8)) / 10
+labnames <- paste0(substr(asapcmultfnames, 2, 5), " Cx",cmult) 
+stpdf <- data.frame(Source = rep(c("Base",  labnames, 
+                                   "Base rho adj", paste(labnames, " C adj")), each=nprojyears),
+                    Year = rep(asap$parms$endyr + 1:nprojyears, 8),
+                    Catch = c(stp, stp1, stp2, stp3, 
+                              stprhoadj, stp1/cmult[1], stp2/cmult[2], stp3/cmult[3]),
+                    Adjusted = rep(c(FALSE, TRUE), each = (4 * nprojyears)))
+
+stpplot <- ggplot(filter(stpdf, Adjusted == FALSE), aes(x=Year, y=Catch, fill=Source)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  ggtitle(paste(asap$parms$endyr, " Catch = ", round(asap$catch.obs[nyears], 0), " mt")) +
+  geom_text(aes(label=round(Catch, 0)), vjust=1.6, color="white", size=3.5, 
+            position=position_dodge(0.9)) +
+  theme_bw()
+print(stpplot)
+ggsave(".\\witch\\short_term_projections.png", stpplot)
+
+stpplotadj <- ggplot(filter(stpdf, Adjusted == TRUE), aes(x=Year, y=Catch, fill=Source)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  ggtitle("When catch adjustments are made to quota") +
+  geom_text(aes(label=round(Catch, 0)), vjust=1.6, color="white", size=3.5, 
+            position=position_dodge(0.9)) +
+  theme_bw()
+print(stpplotadj)
+ggsave(".\\witch\\short_term_projections_adjusted.png", stpplotadj)
+
+
