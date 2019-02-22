0f3d6535d7c55ca3b60a0b16755ed956554fc554
cmlegault <chris.legault@noaa.gov>
2019-02-22 11:41:26 -0500

    run retro plots with fixed PlotASAP (rho label)

--- Rcode/03_plot_witch_retros.R
+++ Rcode/03_plot_witch_retros.R
@@ -58,7 +58,7 @@ tsplot <- ggplot(tsdf, aes(x=Year, y=value, color=Source)) +
 print(tsplot)
 ggsave(".\\witch\\tsplot.png", tsplot)
 
-# get retro plots from ASAPplot (don't know why rho values don't print correctly)
+# get retro plots from ASAPplot 
 windows(record = TRUE)
 PlotRetroWrapper(".\\rundir", paste0(asapfname, "_000"), asap, TRUE, ".\\witch\\", "png")
 dev.off()
