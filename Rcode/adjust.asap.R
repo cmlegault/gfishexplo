98c290be3b3c09ceaadbdc92c84e37a673a25c80
cmlegault <chris.legault@noaa.gov>
2019-02-26 11:28:58 -0500

    add function for cmult with diff selx

--- Rcode/adjust.asap.R
+++ Rcode/adjust.asap.R
@@ -16,3 +16,20 @@ adjust_asap <- function(asap.dat,year,cmult,mmult){
   asap.dat.adj$dat[names(asap.dat$dat) == "M"][[1]] <- m.mat.adj
   return(asap.dat.adj)
 }
+
+# adjusts asap.dat object by setting catch in fleet 2 to cmult time catch in fleet 1 for years >= year
+# assumes starting file already set up with fleet 2 present and selectivity for fleet defined correctly
+adjust_asap2 <- function(asap.dat,year,cmult){
+  catch.mat <- asap.dat$dat[names(asap.dat$dat) == "CAA_mats"]
+  years <- seq(as.numeric(asap.dat$dat[names(asap.dat$dat) == "year1"]),
+               as.numeric(asap.dat$dat[names(asap.dat$dat) == "year1"]) + 
+                 as.numeric(asap.dat$dat[names(asap.dat$dat) == "n_years"]) - 1 , 1)
+  base.catch.mat <- catch.mat[[1]][[1]]
+  catch.mat.adj <- catch.mat[[1]][[2]] * 0 # start with all zeros
+  nc <- length(catch.mat.adj[1,])
+  year.count <- 1:length(years)
+  catch.mat.adj[year.count[years >= year],nc] <- base.catch.mat[year.count[years >= year],nc] * cmult
+  asap.dat.adj <- asap.dat
+  asap.dat.adj$dat[names(asap.dat$dat) == "CAA_mats"][[1]][[2]] <- catch.mat.adj
+  return(asap.dat.adj)
+}
