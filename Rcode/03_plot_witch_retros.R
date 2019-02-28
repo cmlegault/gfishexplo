9b28021e304c4eaf63dd3051cac4814c319c41f9
cmlegault <chris.legault@noaa.gov>
2019-02-28 13:34:27 -0500

    standardize colors

--- Rcode/03_plot_witch_retros.R
+++ Rcode/03_plot_witch_retros.R
@@ -3,9 +3,13 @@
 
 # uncomment following line if need to install latest version of ASAPplots
 # devtools::install_github("cmlegault/ASAPplots", build_vignettes = TRUE)
+library("scales")
 library("ASAPplots")
 library("ggplot2")
 library("dplyr")
+
+# to use consistent colors throughout
+my.col <- hue_pal()(6)
 
 res <- read.csv(".\\witch\\witch_retro_res.csv")
 resfleet <- read.csv(".\\witch\\witch_retro_res_fleet.csv")
@@ -27,6 +31,7 @@ rhoplot <- ggplot(res, aes(x=Cmult, y=SSBrho, color=as.factor(ChangeYear))) +
   geom_point(data = bestres, shape = 1, size=5) +
   geom_hline(yintercept = 0, linetype="dashed") +
   labs(color="Change Year") +
+  scale_color_manual(values = my.col[c(1, 2, 3)]) +
   xlab("Catch Multiplier") +
   theme_bw()
 
@@ -37,6 +42,7 @@ rhoplotfleet <- ggplot(resfleet, aes(x=cmult, y=SSBrho, color=Source)) +
   geom_point() +
   geom_line() +
   geom_point(data = bestresfleet, shape = 1, size=5) +
+  scale_color_manual(values = my.col[c(2, 4, 5)]) +
   geom_hline(yintercept = 0, linetype="dashed") +
   xlab("Catch Multiplier") +
   theme_bw()
@@ -44,22 +50,31 @@ rhoplotfleet <- ggplot(resfleet, aes(x=cmult, y=SSBrho, color=Source)) +
 print(rhoplotfleet)
 ggsave(".\\witch\\rhoplotfleet.png", rhoplotfleet)
 
-# identify the four asap runs
+# identify the asap runs
 asapfname <- "y2010c10m10"
 asapcmultfnames <- paste0("y", bestres$ChangeYear, "c", bestres$Cmult * 10, "m10")
 sourcenames <- paste0(bestres$ChangeYear, " Cx", bestres$Cmult)
+asapfleetfnames <- c(paste0("Young_y", bestresfleet$Year[bestresfleet$Source == "2005 Young"],
+                            "c", bestresfleet$cmult[bestresfleet$Source == "2005 Young"] * 10),
+                     paste0("Old_y", bestresfleet$Year[bestresfleet$Source == "2005 Old"],
+                            "c", bestresfleet$cmult[bestresfleet$Source == "2005 Old"] * 10))
+fleetnames <- paste0(bestresfleet$Source[2:3], "x", bestresfleet$cmult[2:3])
 
 # copy the four base rdat files to witch directory
 shell(paste0("copy .\\rundir\\", asapfname, "_000.rdat .\\witch\\"))
 shell(paste0("copy .\\rundir\\", asapcmultfnames[1], "_000.rdat .\\witch\\"))
 shell(paste0("copy .\\rundir\\", asapcmultfnames[2], "_000.rdat .\\witch\\"))
 shell(paste0("copy .\\rundir\\", asapcmultfnames[3], "_000.rdat .\\witch\\"))
+shell(paste0("copy .\\rundir\\", asapfleetfnames[1], "_000.rdat .\\witch\\"))
+shell(paste0("copy .\\rundir\\", asapfleetfnames[2], "_000.rdat .\\witch\\"))
 
 # read the rdat files
 asap <- dget(paste0(".\\witch\\", asapfname, "_000.rdat"))
 asap1 <- dget(paste0(".\\witch\\", asapcmultfnames[1], "_000.rdat"))
 asap2 <- dget(paste0(".\\witch\\", asapcmultfnames[2], "_000.rdat"))
 asap3 <- dget(paste0(".\\witch\\", asapcmultfnames[3], "_000.rdat"))
+asap4 <- dget(paste0(".\\witch\\", asapfleetfnames[1], "_000.rdat"))
+asap5 <- dget(paste0(".\\witch\\", asapfleetfnames[2], "_000.rdat"))
 
 # plot time series of catch, F, recruits, and SSB
 years <- seq(asap$parms$styr, asap$parms$endyr)
@@ -77,11 +92,33 @@ tsplot <- ggplot(tsdf, aes(x=Year, y=value, color=Source)) +
   geom_line() +
   expand_limits(y=0) +
   facet_wrap(~ metric, scales="free_y") +
+  scale_color_manual(values = my.col[c(1, 2, 3, 6)]) +
   theme_bw() +
   theme(legend.position = "bottom")
 
 print(tsplot)
 ggsave(".\\witch\\tsplot.png", tsplot)
+
+fdf <- data.frame(Year = rep(years, 16),
+                  Source = rep(rep(c("Base", sourcenames[2], fleetnames), each=nyears), 4), 
+                  metric = rep(c("SSB", "F", "Recruits", "Catch"), each = (nyears * 4)),
+                  value = c(asap$SSB, asap2$SSB, asap4$SSB, asap5$SSB,
+                            asap$F.report, asap2$F.report, asap4$F.report, asap5$F.report,
+                            asap$N.age[,1], asap2$N.age[,1], asap4$N.age[,1], asap5$N.age[,1],
+                            asap$catch.obs, asap2$catch.obs, apply(asap4$catch.obs, 2, sum), 
+                            apply(asap5$catch.obs, 2, sum)))
+
+ftsplot <- ggplot(fdf, aes(x=Year, y=value, color=Source)) +
+  geom_point() +
+  geom_line() +
+  scale_color_manual(values = my.col[c(2, 4, 5, 6)]) +
+  expand_limits(y=0) +
+  facet_wrap(~ metric, scales="free_y") +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+print(ftsplot)
+ggsave(".\\witch\\ftsplot.png", ftsplot)
 
 # get retro plots from ASAPplot 
 windows(record = TRUE)
